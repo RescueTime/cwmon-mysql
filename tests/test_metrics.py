3b698f681fb6c3813452c41878e299eb26702957
Hank Gay <hank@rescuetime.com>
2016-10-07 16:12:05 -0400

    chg: test: Use real MySQL connection (as pytest fixture) when testing metrics.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -1,16 +1,27 @@
 # -*- encoding: utf-8 -*-
 """Tests for the various metrics reported by the monitoring CLI."""
-from unittest import mock
+import os
+import pymysql
+import pymysql.cursors
 import pytest
 
 from cwmon_mysql import metrics
 
 
-def _mock_connection():
-    return mock.Mock()
+@pytest.fixture
+def mysql_conn():
+    """A connection to the MySQL DB under test."""
+    return pymysql.connect(
+        host=os.getenv('MYSQL_HOST', '127.0.0.1'),
+        user=os.getenv('MYSQL_USER', 'root'),
+        password=os.getenv('MYSQL_PASSWD', ''),
+        db=os.getenv('MYSQL_DB', ''),
+        port=os.getenv('MYSQL_PORT', 3306),
+        cursorclass=pymysql.cursors.DictCursor
+    )
 
 
-def test_deadlocks_metric_happy_path():
+def test_deadlocks_metric_happy_path(mysql_conn):
     """Create a :class:`~metrics.DeadlocksMetric`."""
-    m = metrics.DeadlocksMetric(_mock_connection())
+    m = metrics.DeadlocksMetric(mysql_conn)
     assert "Innodb deadlocks" == m.unit
