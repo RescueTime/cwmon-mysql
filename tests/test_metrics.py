7916feece68e08fb4f1602bed2e110c47bd8fb89
Hank Gay <hank@rescuetime.com>
2016-10-13 12:02:54 -0400

    new: test: Happy path tests for new metrics.

--- tests/test_metrics.py
+++ tests/test_metrics.py
@@ -25,3 +25,45 @@ def test_deadlocks_metric_happy_path(mysql_conn):
     """Create a :class:`~metrics.DeadlocksMetric`."""
     m = metrics.DeadlocksMetric(mysql_conn)
     assert "Innodb deadlocks" == m.unit
+
+
+def test_uptime_metric_happy_path(mysql_conn):
+    """Create a :class:`~metrics.UptimeMetric`."""
+    m = metrics.UptimeMetric(mysql_conn)
+    assert "Seconds" == m.unit
+
+
+def test_running_threads_metric_happy_path(mysql_conn):
+    """Create a :class:`~metrics.RunningThreadsMetric`."""
+    m = metrics.RunningThreadsMetric(mysql_conn)
+    assert "Threads" == m.unit
+
+
+def test_questions_metric_happy_path(mysql_conn):
+    """Create a :class:`~metrics.QuestionsMetric`."""
+    m = metrics.QuestionsMetric(mysql_conn)
+    assert "Questions" == m.unit
+
+
+def test_slow_queries_metric_happy_path(mysql_conn):
+    """Create a :class:`~metrics.SlowQueriesMetric`."""
+    m = metrics.SlowQueriesMetric(mysql_conn)
+    assert "Queries" == m.unit
+
+
+def test_open_files_metric_happy_path(mysql_conn):
+    """Create a :class:`~metrics.OpenFilesMetric`."""
+    m = metrics.OpenFilesMetric(mysql_conn)
+    assert "Files" == m.unit
+
+
+def test_open_tables_metric_happy_path(mysql_conn):
+    """Create a :class:`~metrics.OpenTablesMetric`."""
+    m = metrics.OpenTablesMetric(mysql_conn)
+    assert "Tables" == m.unit
+
+
+def test_seconds_behind_master_metric_happy_path(mysql_conn):
+    """Create a :class:`~metrics.SecondsBehindMasterMetric`."""
+    m = metrics.SecondsBehindMasterMetric(mysql_conn)
+    assert "Seconds" == m.unit
