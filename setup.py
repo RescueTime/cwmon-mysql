2d534ad6f9701342b8ee3639df73cf216c902b7a
Hank Gay <hank@rescuetime.com>
2016-10-13 15:32:21 -0400

    Bump version: 0.1.0 → 0.2.0

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ def read(*names, **kwargs):
 
 setup(
     name='cwmon-mysql',
-    version='0.1.0',
+    version='0.2.0',
     license='BSD',
     description='A cwmon plugin for monitoring MySQL.',
     long_description='%s\n%s' % (
