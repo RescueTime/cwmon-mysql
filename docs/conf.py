ba0af27529ea2473abfc7ec1fcb3d5fb60f9537e
Hank Gay <hank@rescuetime.com>
2016-10-13 16:46:17 -0400

    Bump version: 0.2.0 → 0.2.1

--- docs/conf.py
+++ docs/conf.py
@@ -24,7 +24,7 @@ project = 'cwmon-mysql'
 year = '2016'
 author = 'Hank Gay'
 copyright = '{0}, {1}'.format(year, author)
-version = release = '0.2.0'
+version = release = '0.2.1'
 
 pygments_style = 'trac'
 templates_path = ['.']
