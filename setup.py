562556633979a0c5c4563cd787e90b0c25864688
Hank Gay <hank@rescuetime.com>
2016-10-18 15:16:44 -0400

    chg: doc: !minor fix up trove classifiers.

--- setup.py
+++ setup.py
@@ -40,8 +40,8 @@ setup(
     zip_safe=False,
     classifiers=[
         # complete classifier list: http://pypi.python.org/pypi?%3Aaction=list_classifiers
-        'Development Status :: 5 - Production/Stable',
-        'Intended Audience :: Developers',
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: System Administrators',
         'License :: OSI Approved :: BSD License',
         'Operating System :: Unix',
         'Operating System :: POSIX',
